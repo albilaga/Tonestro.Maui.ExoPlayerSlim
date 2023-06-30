25162432ca384d4ef14235f147f81e9e36df3cbe
albilaga <albilaga@pm.me>
2023-06-30 16:51:05 +0700

    Add project

---
+++ Tonestro.Maui.ExoPlayerSlim.UsageChecker/MainActivity.cs
@@ -0,0 +1,32 @@
+using Android.Views;
+using Com.Tonestro.Exoplayerslim;
+
+namespace Tonestro.Maui.ExoPlayerSlim.UsageChecker;
+
+[Activity(Label = "@string/app_name", MainLauncher = true)]
+public class MainActivity : Activity
+{
+    private View _videoView;
+    private IExoPlayerSlim _exoPlayerSlim;
+
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+        _videoView = ExoPlayerSlimViewFactory.Create(this);
+        _exoPlayerSlim = ExoPlayerSlimFactory.Create(this,
+            "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8");
+
+        _exoPlayerSlim.AttachPlayerView(_videoView, true, AspectRatios.Fit);
+
+        _exoPlayerSlim.PlayWhenReady = true;
+
+        SetContentView(_videoView);
+
+        _exoPlayerSlim.Play();
+    }
+    protected override void OnDestroy()
+    {
+        _exoPlayerSlim?.Close();
+    }
+}
